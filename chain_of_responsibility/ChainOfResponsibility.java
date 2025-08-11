17606453f3eb733a3973c28d5ccae69160fd0f58
nyanmyr <nyanmarrolpenado@gmail.com>
2025-08-11 19:40:15 +0800

    added chain of responsibility

---
+++ chain_of_responsibility/ChainOfResponsibility.java
@@ -0,0 +1,77 @@
+package chain_of_responsibility;
+
+// request
+class ResourceRequest {
+
+    String resourceType;
+
+    ResourceRequest(String resourceType) {
+        this.resourceType = resourceType;
+    }
+}
+
+// handler
+class ResourceCollector {
+
+    String name;
+    String resourceType;
+    ResourceCollector next;
+
+    ResourceCollector(String name, String resourceType, ResourceCollector next) {
+        this.name = name;
+        this.resourceType = resourceType;
+        this.next = next;
+    }
+
+    public void handleRequest(String resourceType) {
+        if (this.resourceType.equalsIgnoreCase(resourceType)) {
+            System.out.println(name + " is able to deliver the resources.\n");
+            return;
+        } else {
+            System.out.println(name + " is unable to deliver the resources.");
+        }
+
+        if (next != null) {
+            System.out.println(name + " moves the request to " + next.name + ".");
+            next.handleRequest(resourceType);
+        } else {
+            System.out.println("None is able to deliver the resources.");
+        }
+    }
+}
+
+// requestor
+class ConstructionManager {
+
+    String name;
+    ResourceCollector resourceCollector;
+
+    ConstructionManager(String name) {
+        this.name = name;
+    }
+
+    // builds the chain
+    public void addContractors(ResourceCollector resourceCollector) {
+        this.resourceCollector = resourceCollector;
+    }
+
+    public void requestResource(String resourceType) {
+        System.out.println(name + " requests for " + resourceType + ".");
+        resourceCollector.handleRequest(resourceType);
+    }
+}
+
+public class ChainOfResponsibility {
+
+    public static void main(String[] args) {
+        ConstructionManager castleBuilder = new ConstructionManager("Castle Builders Company");
+        castleBuilder.addContractors(new ResourceCollector("Eric's Lumbering Company", "Wood",
+                new ResourceCollector("Baldwin's Mining Company", "Stone",
+                        new ResourceCollector("Smith's Steel Company", "Steel", null))));
+
+        castleBuilder.requestResource("wood");
+        castleBuilder.requestResource("stone");
+        castleBuilder.requestResource("steel");
+        castleBuilder.requestResource("magic stones");
+    }
+}
